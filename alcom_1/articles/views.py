959201b767f1c72c4f40fd07f27c2bf7a5b53bb3
m1stikal <m1stiklouis@gmail.com>
2017-08-17 12:58:30 +0200

    view1

--- alcom_1/articles/views.py
+++ alcom_1/articles/views.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
+def hello(request):
+    name = "Mike"
+    html - "<html><body>Hi %s. this seems to have worked!</body></html>" % name
+    return HttpResponse(html)
+
 # Create your views here.
