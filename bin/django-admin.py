a7326b164980aed7f2d14b67b6c0a5273966f925
m1stikal <m1stiklouis@gmail.com>
2017-08-17 11:07:56 +0200

    First commit

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/servadmin/django-clean/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.11.4','django-admin.py'
+__requires__ = 'Django==1.11.4'
+__import__('pkg_resources').run_script('Django==1.11.4', 'django-admin.py')
