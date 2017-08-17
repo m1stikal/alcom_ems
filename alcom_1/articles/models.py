98375382a6b940862bc784a26cd789b022e0c4f5
m1stikal <m1stiklouis@gmail.com>
2017-08-17 11:49:44 +0200

    Up from laptop

--- alcom_1/articles/models.py
+++ alcom_1/articles/models.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Article(models.Model);:
+        title = models.TextField(max_length=254)
+        body = models.TextField()
+
 # Create your models here.
